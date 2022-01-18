b769afc73ebd57ece45c5215db53b3a5bca58d8c
AnthonySzymczak <doyalajs1@live.com>
2022-01-18 16:05:43 -0500

    finished user inputs

--- UserInputs/UserInputs/Program.cs
+++ UserInputs/UserInputs/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Type in your name on the following line.");
+String name = Console.ReadLine();
+
+Console.WriteLine("Type in your age on the following line.");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Hello "+ name);
+Console.WriteLine("You are " + age +" years old!");
