ba6275b0cb87f41d6f12daf9dd83235351377d1b
AnthonySzymczak <doyalajs1@live.com>
2022-01-18 16:21:07 -0500

    basic arithmetic completed

--- BasicArithmetic/BasicArithmetic/Program.cs
+++ BasicArithmetic/BasicArithmetic/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+int friends = 10;
+
+// Addition
+
+// friends = friends + 2;
+// friends += 2;
+// friends++;          increments by 1
+
+
+// Subtraction
+
+// friends = friends - 1;
+// friends -= 1;
+// friends--;          decrements by 1  
+
+
+// Multiplication
+
+// friends = friends * 2;
+// firends *= 2;
+
+
+// Division  ***Be Warned of integer division! it will return a whole number unless declaration is switched to a double
+
+// friends = friends / 2;
+// firends /= 2;
+
+int remainder = friends % 3;
+
+
+Console.WriteLine(remainder);
