1e473e3ef4a01f9f63d6aef4ba3fd3d32450f63d
AnthonySzymczak <doyalajs1@live.com>
2022-01-19 10:27:33 -0500

    .ToUpper

---
+++ UsefulStringMethods/UsefulStringMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+String fullName = "Anthony Szymczak";
+
+//displays full name as uppercase
+fullName = fullName.ToUpper();
+
+Console.WriteLine(fullName);
