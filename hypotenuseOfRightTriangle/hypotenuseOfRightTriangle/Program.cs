38df6ea2d788329600f3443ed5effd9412864a6d
AnthonySzymczak <doyalajs1@live.com>
2022-01-19 10:24:49 -0500

    prompts user for 2 sides then finds the hypotenuse

---
+++ hypotenuseOfRightTriangle/hypotenuseOfRightTriangle/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//prompt user to enter the first side of a triangle
+Console.WriteLine("Enter side A:");
+double a = Convert.ToDouble(Console.ReadLine());
+
+//prompt user to enter the second side of a triangle
+Console.WriteLine("Enter side B:");
+double b = Convert.ToDouble(Console.ReadLine());
+
+
+//calculations for finding the remaining side of the triangle (hypotenuse)
+double c = Math.Sqrt((a * a) + (b * b));
+Console.WriteLine("The Hypotenuse is: " + c);
