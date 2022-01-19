05393bdb1885477227006d0e93c06ed4f0f8a983
AnthonySzymczak <doyalajs1@live.com>
2022-01-19 09:33:38 -0500

    .pow and sqrt

---
+++ LearningMathClass/LearningMathClass/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+double x = 3;
+
+//raises x to the second power returning 9
+double a = Math.Pow(x, 2);
+
+// finds the square root of a number
+double b = Math.Sqrt(x);
+
+
+Console.WriteLine(a);
+Console.WriteLine(b);
